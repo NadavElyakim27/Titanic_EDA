62429eeb661c422b87945892e9c5bb99cad23ab1
Nadav Elyakim <27nadav@gmail.com>
2024-02-14 17:11:44 +0200

    add Plot train and test loss and accuracy after train the model

--- src/model.py
+++ src/model.py
@@ -5,6 +5,8 @@ This module contains the implementation of a LogisticRegression model for classi
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+from src.evaluation import accuracy_clac
 
 class LogisticRegression:
     """
@@ -12,6 +14,7 @@ class LogisticRegression:
     """
 
     def __init__(self,
+                  data: dict = None,
                   learning_rate: float=0.01,
                   n_iterations: int=1000,
                   regularization: str='l2',
@@ -23,7 +26,13 @@ class LogisticRegression:
             learning_rate: learning rate of gradient descent
             n_iterations (int): max iterations for gradient descent
         """
-
+        # Data
+        self.data = data
+        self.X_train = data['X_train']
+        self.X_test = data['X_test']
+        self.y_train = data['y_train']
+        self.y_test = data['y_test']
+        # Param
         self.learning_rate = learning_rate
         self.n_iterations = n_iterations
         self.regularization = regularization
@@ -42,43 +51,79 @@ class LogisticRegression:
         """
 
         return 1 / (1 + np.exp(-z))
+    
+    def loss_clac(self, y_true, y_pred):
+        """
+        Compute binary cross entropy loss
+        Args:
+            y_true: true label
+            y_pred: predicted probabilities
+        Returns:
+            loss: binary cross entropy
+        """        
 
-    def fit(self, X, y):
+        epsilon = 1e-9
+        y1 = y_true * np.log(y_pred + epsilon)
+        y2 = (1-y_true) * np.log(1 - y_pred + epsilon)
+        loss = -np.mean(y1 + y2)
+        
+        return loss
+        
+    def fit(self, plot: bool = False):
         """
         Fit the logistic regression model to the training data
-        Args:
-            X: array-like of shape (n_samples, n_features)
-            y: array-like of shape (n_samples,)
+        Returns:
+            train and test losses
         """
+        # Losses and accuracy to memory lists
+        train_losses = []
+        test_losses = []
+        train_acc = []
+        test_acc = []
 
         # Initialize weights and bias
-        self.weights = np.zeros(X.shape[1])
+        self.weights = np.zeros(self.X_train.shape[1])
         self.bias = 0
 
         for _ in range(self.n_iterations):
             # Calculate X*T + b
-            z = np.dot(X, self.weights) + self.bias
+            z = np.dot(self.X_train, self.weights) + self.bias
 
             # Apply sigmoid function to get probabilities
-            probabilities = self.sigmoid(z)
+            y_perd = self.sigmoid(z)
+
+            # Predict for accuracy
+            y_train_pred = self.predict(self.X_train)
+            y_pred_test = self.predict(self.X_test)
+            # A losses and accuracy to memory lists
+            train_losses.append(self.loss_clac(self.y_train, y_perd))
+            test_losses.append(self.loss_clac(self.y_test, y_pred_test))
+            train_acc.append(accuracy_clac(self.y_train, y_train_pred))
+            test_acc.append(accuracy_clac(self.y_test, y_pred_test))
 
             # Gradient
-            gradient_weights = np.dot(X.T, (probabilities - y)) / len(y)
-            gradient_bias = np.sum(probabilities - y) / len(y)
+            dw = np.dot(self.X_train.T, (y_perd - self.y_train)) / len(self.y_train)
+            db = np.sum(y_perd - self.y_train) / len(self.y_train)
 
             # Update weights and bias using gradient descent
-            self.weights -= self.learning_rate * gradient_weights
-            self.bias -= self.learning_rate * gradient_bias
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
 
-        self.feature_importances_ =  np.abs(self.weights) 
+        # Save feature importances
+        self.feature_importances_ =  np.abs(self.weights)
 
+        # Plot if need
+        if plot: plot_losses_accuracy((train_losses, test_losses), (train_acc,test_acc))
+
+        return (train_losses, test_losses)
+    
     def predict(self, X):
         """
         Predict binary labels for the input data.
         Args:
             X: array-like of shape (n_samples, n_features)
         Returns:
-            binary_predictions: Array of predicted binary labels
+            y_perd: Array of predicted binary labels
         """
         
         # Calculate X*T + b
@@ -88,7 +133,7 @@ class LogisticRegression:
         predictions = self.sigmoid(z)
 
         # Convert probabilities to binary predictions
-        binary_predictions = np.round(predictions).astype(int)
+        y_perd = np.round(predictions).astype(int)
 
         # Apply regularization
         if self.regularization == 'l1':
@@ -96,5 +141,38 @@ class LogisticRegression:
         elif self.regularization == 'l2':
             self.weights -= 2 * self.lambda_reg * self.weights
 
-        
-        return binary_predictions
+        return y_perd
+    
+
+def plot_losses_accuracy(losses, accuracy):
+    """
+    Plot train and test loss and accuracy after train the model
+    Args:
+        losses: taple - train loss, test loss
+        accuracy: taple - accuracy loss, accuracy loss
+    Returns:
+        plot
+    """
+    fig, axs = plt.subplots(2, 2, figsize = (14,12))
+
+    # Train loss plot
+    axs[0][0].plot(losses[0], label='Training Loss')
+    axs[0][0].set_xlabel('Epochs')
+    axs[0][0].set_ylabel('Loss')
+    axs[0][0].set_title('Training Loss over Epochs')
+    # Test loss plot
+    axs[0][1].plot(losses[1], label='Test Loss')
+    axs[0][1].set_xlabel('Epochs')
+    axs[0][1].set_title('Test Loss over Epochs')
+
+    # Train accuracy plot
+    axs[1][0].plot(accuracy[0], label='Training accuracy')
+    axs[1][0].set_xlabel('Epochs')
+    axs[1][0].set_ylabel('accuracy')
+    axs[1][0].set_title('Training accuracy over Epochs')
+    # Test accuracy plot
+    axs[1][1].plot(accuracy[1], label='Test accuracy')
+    axs[1][1].set_xlabel('Epochs')
+    axs[1][1].set_title('Test accuracy over Epochs')
+
+    plt.show()
